ebc891aa9a3cae74fcc49de55ce1929b1d515d0d
Rhys van der Waerden <rhys.vdw@gmail.com>
2023-03-12 16:11:41 +1100

    Rename

--- Assets/_Game/Scripts/Ecs/Systems/MoveSystem.cs
+++ Assets/_Game/Scripts/Ecs/Systems/MoveSystem.cs
@@ -5,20 +5,20 @@ namespace Frog {
     EcsWorld _world;
     EcsFilter _filter;
     EcsPool<Move> _moves;
-    EcsPool<Body> _transforms;
+    EcsPool<Body> _actors;
 
     public void PreInit(IEcsSystems systems) {
       _world = systems.GetWorld();
       _filter = _world.Filter<Move>().End();
       _moves = _world.GetPool<Move>();
-      _transforms = _world.GetPool<Body>();
+      _actors = _world.GetPool<Body>();
     }
 
     public void Run(IEcsSystems systems) {
       foreach (var entity in _filter) {
         ref var m = ref _moves.Get(entity);
-        ref var t = ref _transforms.Get(entity);
-        t.Position += m.Direction;
+        ref var a = ref _actors.Get(entity);
+        a.Position += m.Direction * a.Config.Speed;
       }
     }
   }
